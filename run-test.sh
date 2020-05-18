e6759ec7c112db41267de4b767852d524f17252e
Marthen Luther <marthenlt@gmail.com>
2020-05-18 15:59:08 +0800

    update run-test.sh to add more load
    Changed test script to add more stress. Running 10 tests with 20 million iterations.

--- run-test.sh
+++ run-test.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-echo "3 testings, each with 20,000,000 (20 millions) iterations"
+echo "10 testings, each with 20,000,000 (20 millions) iterations and followed by 0.5 second sleep"
 
+echo "Test #1:"
 time curl http://localhost:8080/graal/benchmark/20000000
-sleep 1
+sleep 0.5
+echo "Test #2:"
 time curl http://localhost:8080/graal/benchmark/20000000
-sleep 1 
+sleep 0.5
+echo "Test #3:"
 time curl http://localhost:8080/graal/benchmark/20000000
-
+sleep 0.5
+echo "Test #4:"
+time curl http://localhost:8080/graal/benchmark/20000000
+sleep 0.5
+echo "Test #5:"
+time curl http://localhost:8080/graal/benchmark/20000000
+sleep 0.5
+echo "Test #6:"
+time curl http://localhost:8080/graal/benchmark/20000000
+sleep 0.5
+echo "Test #7:"
+time curl http://localhost:8080/graal/benchmark/20000000
+sleep 0.5
+echo "Test #8:"
+time curl http://localhost:8080/graal/benchmark/20000000
+sleep 0.5
+echo "Test #9:"
+time curl http://localhost:8080/graal/benchmark/20000000
+sleep 0.5
+echo "Test #10:"
+time curl http://localhost:8080/graal/benchmark/20000000
