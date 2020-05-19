a22cecdecd3017d835df2e775e4fc5f0b9a7f745
Marthen Luther <marthenlt@gmail.com>
2020-05-19 17:01:02 +0800

    adding --latency for wrk

--- run-test.sh
+++ run-test.sh
@@ -2,12 +2,10 @@
 
 echo "This test script is by default will use wrk2, but if wrks is not installed it will then execute using curl"
 
-# type wrk >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
-
 if type wrk 2>/dev/null; then 
 	echo "wrk is present in the system. Testing will continue using wrk"
-	echo "launching wrk using this options: wrk -t2 -c10 -d300s -R2000"
-	wrk -t2 -c10 -d300s -R2000 http://localhost:8080/graal/benchmark/20000000
+	echo "launching wrk using this options: wrk -t2 -c10 -d300s -R200 --latency"
+	wrk -t2 -c10 -d300s -R200 --latency http://localhost:8080/graal/benchmark/20000000
 else
 	echo "Using curl"
 	echo "10 curl testings, each with 20,000,000 (20 millions) iterations and followed by 0.5 second sleep"
