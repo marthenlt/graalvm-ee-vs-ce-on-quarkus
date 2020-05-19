72585806a81e3d8822ceacb5ab0c6ffef8553500
Marthen Luther <marthenlt@gmail.com>
2020-05-19 15:25:13 +0800

    add xmx and xms in the startup scripts for each JVM, as well as by default will use wrk if wrk is already installed, otherwise it will still use curl

--- graal-ce-jfr.sh
+++ graal-ce-jfr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Continuous recording
-$GRAALVM_HOME/bin/java -Dgraal.CompilerConfiguration=community -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:StartFlightRecording=name=GraalCEBenchmark,settings=profile -XX:FlightRecorderOptions=defaultrecording=true,dumponexit=true,loglevel=info,repository=.,dumponexitpath=./graal-ce.jfr  -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -jar target/getting-started-1.0-SNAPSHOT-runner.jar
+$GRAALVM_HOME/bin/java -Xms256m -Xmx256m -Dgraal.CompilerConfiguration=community -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:StartFlightRecording=name=GraalCEBenchmark,settings=profile -XX:FlightRecorderOptions=defaultrecording=true,dumponexit=true,loglevel=info,repository=.,dumponexitpath=./graal-ce.jfr  -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -jar target/getting-started-1.0-SNAPSHOT-runner.jar
 
 
--- run-test.sh
+++ run-test.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
 
-echo "10 testings, each with 20,000,000 (20 millions) iterations and followed by 0.5 second sleep"
+echo "This test script is by default will use wrk2, but if wrks is not installed it will then execute using curl"
 
-echo "Test #1:"
-time curl http://localhost:8080/graal/benchmark/20000000
-sleep 0.5
-echo "Test #2:"
-time curl http://localhost:8080/graal/benchmark/20000000
-sleep 0.5
-echo "Test #3:"
-time curl http://localhost:8080/graal/benchmark/20000000
-sleep 0.5
-echo "Test #4:"
-time curl http://localhost:8080/graal/benchmark/20000000
-sleep 0.5
-echo "Test #5:"
-time curl http://localhost:8080/graal/benchmark/20000000
-sleep 0.5
-echo "Test #6:"
-time curl http://localhost:8080/graal/benchmark/20000000
-sleep 0.5
-echo "Test #7:"
-time curl http://localhost:8080/graal/benchmark/20000000
-sleep 0.5
-echo "Test #8:"
-time curl http://localhost:8080/graal/benchmark/20000000
-sleep 0.5
-echo "Test #9:"
-time curl http://localhost:8080/graal/benchmark/20000000
-sleep 0.5
-echo "Test #10:"
-time curl http://localhost:8080/graal/benchmark/20000000
+# type wrk >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
+
+if type wrk 2>/dev/null; then 
+	echo "wrk is present in the system. Testing will continue using wrk"
+	echo "launching wrk using this options: wrk -t2 -c10 -d300s -R2000"
+	wrk -t2 -c10 -d300s -R2000 http://localhost:8080/graal/benchmark/20000000
+else
+	echo "Using curl"
+	echo "10 curl testings, each with 20,000,000 (20 millions) iterations and followed by 0.5 second sleep"
+
+	echo "Test #1:"
+	time curl http://localhost:8080/graal/benchmark/20000000
+	sleep 0.5
+	echo "Test #2:"
+	time curl http://localhost:8080/graal/benchmark/20000000
+	sleep 0.5
+	echo "Test #3:"
+	time curl http://localhost:8080/graal/benchmark/20000000
+	sleep 0.5
+	echo "Test #4:"
+	time curl http://localhost:8080/graal/benchmark/20000000
+	sleep 0.5
+	echo "Test #5:"
+	time curl http://localhost:8080/graal/benchmark/20000000
+	sleep 0.5
+	echo "Test #6:"
+	time curl http://localhost:8080/graal/benchmark/20000000
+	sleep 0.5
+	echo "Test #7:"
+	time curl http://localhost:8080/graal/benchmark/20000000
+	sleep 0.5
+	echo "Test #8:"
+	time curl http://localhost:8080/graal/benchmark/20000000
+	sleep 0.5
+	echo "Test #9:"
+	time curl http://localhost:8080/graal/benchmark/20000000
+	sleep 0.5
+	echo "Test #10:"
+	time curl http://localhost:8080/graal/benchmark/20000000
+fi
